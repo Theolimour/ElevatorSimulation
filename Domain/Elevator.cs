5333aea79794924a90209c59e5b6f2fd02d7a480
theolimour <motheo@pulego.co.za>
2024-07-30 14:54:54 +0200

    Add project files.

---
+++ Domain/Elevator.cs
@@ -0,0 +1,24 @@
+﻿namespace ElevatorSimulation.Domain
+{
+    public class Elevator
+    {
+        public int Id { get; set; }
+        public int CurrentFloor { get; set; }
+        public bool IsMoving { get; set; }
+        public string Direction { get; set; }
+        public int PassengerCount { get; set; }
+        public int MaxCapacity { get; set; }
+
+        public Elevator(int id, int maxCapacity)
+        {
+            Id = id;
+            MaxCapacity = maxCapacity;
+            CurrentFloor = 0; // Assuming ground floor as the initial floor
+            IsMoving = false;
+            Direction = "Stationary";
+            PassengerCount = 0;
+        }
+
+        
+    }
+}
